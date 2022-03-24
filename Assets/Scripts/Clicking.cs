e72911c3a5a289056cdc48df7002e1eac5a499f2
Shreyansh911 <shreyanshvarma01@gmail.com>
2022-03-24 23:53:51 +0530

    Initial commit

---
+++ Assets/Scripts/Clicking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clicking : MonoBehaviour
+{
+    [SerializeField] Button _button;
+    [SerializeField] Text _buttonText;
+    [SerializeField] string _playerSide;
+
+    GameManager _gameManager;
+
+
+    public void GameManagerRef(GameManager GameManager)
+    {
+        _gameManager = GameManager;
+    }
+
+    public void SetSpace()
+    {
+        _buttonText.text = _playerSide;
+        _button.interactable = false;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text[] _texts;
+
+    void Awake()
+    {
+        GameManagerRef();
+    }
+
+    void GameManagerRef()
+    {
+        for (int i = 0; i < _texts.Length; i++)
+        {
+            _texts[i].GetComponentInParent<Clicking>().GameManagerRef(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return "?";
+    }
+
+    public void EndTurn()
+    {
+        Debug.Log("EndTurn is not implemented!");
+    }
+}
+
+
